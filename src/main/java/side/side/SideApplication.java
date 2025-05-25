d29f8d70f747b62a507baae82d07de01dd1430ea
hy-rf <heyu880202@gmail.com>
2025-05-25 21:47:08 +0800

    debug: add print stack trace function

--- src/main/java/side/side/SideApplication.java
+++ src/main/java/side/side/SideApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SideApplication {
 
 	public static void main(String[] args) {
+		try {
 		SpringApplication.run(SideApplication.class, args);
-	}
+		} catch (Exception e) {
+			e.printStackTrace();
+        }
+    }
 
 }
