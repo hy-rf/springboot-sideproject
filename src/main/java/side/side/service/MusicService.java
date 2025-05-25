19f0a6a36f12579ce7c090cb378d6142955b4745
hy-rf <heyu880202@gmail.com>
2025-05-25 21:46:40 +0800

    chore: add MusicService

---
+++ src/main/java/side/side/service/MusicService.java
@@ -0,0 +1,10 @@
+package side.side.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MusicService {
+    public void uploadMusic(){
+        System.out.println("Upload Success!");
+    }
+}
